06bf23236512f3bdbadb36e016d0b0e95b68d8b5
handsomemolly <ryan.kendig@gmail.com>
2021-02-05 11:19:57 -0800

    space dudez

--- src/App.js
+++ src/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import { render } from "enzyme";
+import React from "react";
+
+class App extends React.Component {
+  state = {
+    spaceDudes: [],
+  };
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+      .then((res) => res.json())
+      .then((json) =>
+        this.setState({
+          spaceDudes: json.people,
+        })
+      );
+  }
+
+  render() {
+    return <div>{this.state.spaceDudes.map((dude) => dude.name)}</div>;
+  }
+}
+
+export default App;
